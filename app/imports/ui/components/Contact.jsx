a34f34c33bc2f9ac5879e60f59e9fa07bec3559b
PhatCa <phatca@hawaii.edu>
2024-04-01 22:37:06 -1000

    finish in 20 mins

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+import Note from './Note';
+import AddNote from './AddNote';
 /** Renders a single row in the List Stuff table. See pages/ListContacts.jsx. */
-const Contact = ({ contact }) => (
-  <Card className="h-100">
-    <Card.Header>
-      <Image src={contact.image} width={75} />
-      <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
-      <Card.Subtitle>{contact.address}</Card.Subtitle>
-    </Card.Header>
-    <Card.Body>
-      <Card.Text>
-        {contact.description}
-      </Card.Text>
-      <Link to={`/edit/${contact._id}`}>Edit</Link>
-    </Card.Body>
-  </Card>
+const Contact = ({ contact, notes }) => (
+    <Card className="h-100">
+      <Card.Header>
+        <Image src={contact.image} width={75} />
+        <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+        <Card.Subtitle>{contact.address}</Card.Subtitle>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>
+          {contact.description}
+        </Card.Text>
+        <ListGroup variant="flush">
+          {notes.map((note) => <Note key={note._id} note={note} />)}
+        </ListGroup>
+        <AddNote owner={contact.owner} contactId={contact._id} />
+        <Link to={`/edit/${contact._id}`}>Edit</Link>
+      </Card.Body>
+    </Card>
 );
 
 // Require a document to be passed to this component.
@@ -27,8 +33,16 @@ Contact.propTypes = {
     address: PropTypes.string,
     description: PropTypes.string,
     image: PropTypes.string,
+    owner: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
+  notes: PropTypes.arrayOf(PropTypes.shape({
+    note: PropTypes.string,
+    contactID: PropTypes.string,
+    owner: PropTypes.string,
+    createdAt: PropTypes.instanceOf(Date),
+    _id: PropTypes.string,
+  })).isRequired,
 };
 
 export default Contact;
